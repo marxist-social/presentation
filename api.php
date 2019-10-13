f60f977263752c31b37dfdf04b6182f5f967af75
Juniper McIntyre <junipermcintyre@gmail.com>
2019-10-13 10:55:48 -0400

    See whats happening on the server

--- api.php
+++ api.php
@@ -1,6 +1,9 @@
 <?php
 if (isset($_POST['slide'])) {
-	file_put_contents(__DIR__.'/meta.json', json_encode(['slide' => $_POST['slide']]));
+	$success = file_put_contents(__DIR__.'/meta.json', json_encode(['slide' => $_POST['slide']]));
+	var_dump($success);
+	echo __DIR__.'/meta.json';
+	echo "\n";
 	echo "good";
 } else {
 	$slide = json_decode(file_get_contents(__DIR__.'/meta.json'), true);
