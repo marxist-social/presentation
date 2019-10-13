63212d8f7324964485f81ef1f65ad4311b61d1b8
Juniper McIntyre <junipermcintyre@gmail.com>
2019-10-13 10:57:49 -0400

    It was file permissions ofc

--- api.php
+++ api.php
@@ -1,12 +1,15 @@
 <?php
 if (isset($_POST['slide'])) {
 	$success = file_put_contents(__DIR__.'/meta.json', json_encode(['slide' => $_POST['slide']]));
-	var_dump($success);
-	echo __DIR__.'/meta.json';
-	echo "\n";
-	echo "good";
+
+	if ($success) {
+		echo "good";
+	} else {
+		echo "bad";
+	}
 } else {
 	$slide = json_decode(file_get_contents(__DIR__.'/meta.json'), true);
+
 	if ($slide && isset($slide['slide'])) {
 		echo $slide['slide'];
 	} else {
